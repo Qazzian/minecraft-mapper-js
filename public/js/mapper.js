a59980060686e12c910d8ac6ed3b5b166e3bea44
Qazzian <Qazzian@gmail.com>
2017-03-10 18:15:57 +0000

    Fix the canvas sometimes being offset on page reload
    I think this was because the page had been scrolled after shrinking the window

--- public/js/mapper.js
+++ public/js/mapper.js
@@ -26,6 +26,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.5, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+window.scroll(0, 0);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
