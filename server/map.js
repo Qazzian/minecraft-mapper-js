df89c1220bad583d5fda1b8ba3ad58f0b2b00bae
Qazzian <Qazzian@gmail.com>
2019-07-04 09:21:57 +0100

    Loading map meta data

--- server/map.js
+++ server/map.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const nbt = require('prismarine-nbt');
+const worldFactory = require('prismarine-world');
+
+const MAP_PATHS = {
+	region: 'region',
+};
 
 class Map {
 	constructor(mapName, config) {
@@ -9,22 +14,46 @@ class Map {
 		this.config = config;
 	}
 
-
 	async loadMetaData(name, dirBuilder) {
 		const mapDir = dirBuilder(name);
 		const mapPath = path.join(mapDir, "/level.dat");
 
 		return new Promise((resolve, reject) => {
 			fs.readFile(mapPath, (err, fileData) => {
-				if (err) {reject(err);}
+				if (err) {
+					reject(err);
+				}
 
 				nbt.parse(fileData, function (error, nbtData) {
-					if (error) { reject(error); }
+					if (error) {
+						reject(error);
+					}
 					resolve(nbtData);
 				});
 			});
 		});
 	}
+
+	static getMapPath(mapId, rootPath, subDir) {
+		const mapPath = path.join(rootPath, mapId);
+		if (subDir in MAP_PATHS) {
+			return path.join(mapPath, MAP_PATHS[subDir]);
+		}
+		return mapPath;
+	}
+
+	static getVersionString(metaData) {
+		return metaData.value.Data.value.Version.value.Name.value;
+	}
+
+	static getVersionId(metaData) {
+		return metaData.value.Data.value.Version.value.Id.value;
+	}
+
+	static getChunk() {
+		return {};
+	}
+
 }
 
 module.exports = Map;
--- server/map.test.js
+++ server/map.test.js
@@ -8,15 +8,23 @@ const mapConfig = {
 	}
 };
 
+const mapRoot = path.join(__dirname, "../map/");
+
 describe('Map', () => {
 	test('To be defined', () => {
 		expect(Map).toBeDefined();
 	});
 
-	test('loading a file', async () => {
+	test('loading a PC/Java Map', async () => {
 		const map = new Map('Qazzian1', mapConfig);
 		expect(map).toBeDefined();
 		const mapData = await map.loadMetaData('Qazzian1', mapConfig.getMapDir);
-		console.info('mapData: ', JSON.stringify(mapData));
+		expect(Map.getVersionString(mapData)).toBe('1.13.2');
+		expect(Map.getVersionId(mapData)).toBe(1631);
+	});
+
+
+	test('Load block data', () => {
+		expect(Map.getChunk()).toMatchObject({});
 	});
 });
